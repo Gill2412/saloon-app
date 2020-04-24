736245516bde901cb03f3c98b30be2d488109238
Gill2412 <gurjindergill8@gmail.com>
2020-04-24 23:20:32 +0500

    mongopath chaneg

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose')
 const db = require('./config')
-mongoose.connect(db.mongoURI,{
+mongoose.connect("mongodb+srv://test:test@cluster0-mtmq8.mongodb.net/test?retryWrites=true&w=majority",{
   useNewUrlParser: true,
   useCreateIndex: true,
   useFindAndModify: false,
